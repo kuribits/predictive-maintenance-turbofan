c1f27eb4b821083651196e729befe05c455c181f
Ubuntu <ubuntu@ip-172-31-40-217.us-east-2.compute.internal>
2020-08-04 05:15:09 +0000

    use rsync

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/matthiaslau/Turbofan-Federated-Learning-POC.git
-mv *.ipynb ./Turbofan-Federated-Learning-POC
+rsync -avzh --remove-source-files src/ Turbofan-Federated-Learning-POC
 cd ./Turbofan-Federated-Learning-POC
 python data_preprocessor.py --worker_count 1
