3fa99c2632b419691dcce12c826d1c899ed703bb
Ubuntu <ubuntu@ip-172-31-40-217.us-east-2.compute.internal>
2020-08-04 07:09:20 +0000

    concised distr data ipynb

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 echo "Cloning original Turbofan POC repository..."
 git clone https://github.com/matthiaslau/Turbofan-Federated-Learning-POC.git
+echo "Merging improved files into cloned repository..."
+rsync -avzhP federated-trainer Turbofan-Federated-Learning-POC/federated-trainer
 echo "Downloading and preprocessing data..."
 cd ./Turbofan-Federated-Learning-POC && python data_preprocessor.py --worker_count 1
 echo "Initialisation done."
