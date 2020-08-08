1eb9ea0f429992fddab68b17918743d81181d399
kuribits <xtinewhy@gmail.com>
2020-08-08 04:46:34 +0000

    rejig config, dataloading, concise models

--- models.py
+++ models.py
@@ -1,45 +1,48 @@
 from torch import nn
 import torch.nn.functional as F
+import syft.frameworks.torch.nn.rnn as syftnn
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.xavier_uniform(m.weight)
+        m.bias.data.fill_(0.01)
 
 
-class TurbofanModel(nn.Module):
-    def __init__(self, train_mean, train_std, input_size):
+class VanillaGRU(nn.Module):
+    def __init__(self, window_size, features_size):
         super().__init__()
-
-        self.train_mean = train_mean
-        self.train_std = train_std
-
-        self.fc1 = nn.Linear(input_size, 24)
-        self.fc2 = nn.Linear(24, 24)
-        self.fc3 = nn.Linear(24, 24)
-        self.fc4 = nn.Linear(24, 1)
-
-    # extend send function to also send the mean and std params
-    def send(self, worker):
-        super().send(worker)
-        self.train_mean = self.train_mean.send(worker)
-        self.train_std = self.train_std.send(worker)
-
-    # extend get function to also get the mean and std params
-    def get(self):
-        super().get()
-        self.train_mean = self.train_mean.get()
-        self.train_std = self.train_std.get()
+        self.bn1 = nn.BatchNorm1d(window_size)
+        self.lstm = syftnn.GRU(input_size=features_size, hidden_size=1, batch_first=True)
+        self.fc1 = nn.Linear(80, 1)
 
     def forward(self, x):
-        # scale the input
-        x = (x - self.train_mean) / self.train_std
-
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = F.relu(self.bn1(x))
+        x = F.relu(self.lstm(x)[0])
+        x = x.permute(0,2,1)
+        x = self.fc1(x)
         x = x[:, -1, :]
 
         return x
 
 
-class BatchNormFCModel(nn.Module,):
+class VanillaLSTM(nn.Module):
+    def __init__(self, window_size, features_size):
+        super().__init__()
+        self.bn1 = nn.BatchNorm1d(window_size)
+        self.lstm = syftnn.LSTM(input_size=features_size, hidden_size=1, batch_first=True)
+        self.fc1 = nn.Linear(80, 1)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(x))
+        x = F.relu(self.lstm(x)[0])
+        x = x.permute(0,2,1)
+        x = self.fc1(x)
+        x = x[:, -1, :]
+
+        return x
+
+
+class BatchNormFCModel(nn.Module):
     def __init__(self, window_size, features_size):
         super().__init__()
         self.bn1 = nn.BatchNorm1d(window_size)
@@ -47,14 +50,6 @@ class BatchNormFCModel(nn.Module,):
         self.fc2 = nn.Linear(24, 24)
         self.fc3 = nn.Linear(24, 24)
         self.fc4 = nn.Linear(24, 1)
-
-    # extend send function to also send the mean and std params
-    def send(self, worker):
-        super().send(worker)
-
-    # extend get function to also get the mean and std params
-    def get(self):
-        super().get()
 
     def forward(self, x):
         x = F.relu(self.bn1(x))
